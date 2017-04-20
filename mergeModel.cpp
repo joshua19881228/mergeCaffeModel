953b759000a9d09f12a44ff846c772a8242692e8
Joshua Li <lizhixuan_1988@126.com>
2017-04-20 20:48:39 +0800

    update code

--- mergeModel.cpp
+++ mergeModel.cpp
@@ -2,7 +2,7 @@
 #include <strstream>
 using std::strstream;
 
-bool splitPair(const string input, SrcDstPair& a_pair)
+bool MergeModelClass::splitPair(const string input, SrcDstPair& a_pair)
 {
     int first_idx = input.find_first_of(":");
     int last_idx = input.find_last_of(":");
@@ -19,7 +19,7 @@ bool splitPair(const string input, SrcDstPair& a_pair)
     }
 }
 
-bool splitSrcLine(const string input, SrcInfo& a_src_info)
+bool MergeModelClass::splitSrcLine(const string input, SrcInfo& a_src_info)
 {
     strstream a_stream;
     vector<string> splits;
@@ -62,7 +62,7 @@ bool splitSrcLine(const string input, SrcInfo& a_src_info)
     return true;
 }
 
-bool loadConfigFile(const string config_file_path, ConfigInfo& config_info)
+bool MergeModelClass::loadConfigFile(const string config_file_path)
 {
     std::ifstream config_file(config_file_path.c_str());
     int set_num = 0;
@@ -93,5 +93,124 @@ bool loadConfigFile(const string config_file_path, ConfigInfo& config_info)
     }
     LOG(INFO) << "Config file loaded";
     config_file.close();
+    return true;
+}
+
+bool MergeModelClass::initDstModel()
+{
+    dst_net = new Net<float>(config_info.dst_prototxt_path, caffe::TRAIN);
+    if (dst_net == NULL)
+        return false;
+    FILE* file = fopen(config_info.dst_model_path.c_str(), "r");
+    if (file == NULL)
+        LOG(WARNING) << "dst model initialized from empty weights";
+    else
+    {
+        dst_net->CopyTrainedLayersFrom(config_info.dst_model_path);
+        fclose(file);
+    }        
+    return true;
+}
+
+bool MergeModelClass::initSrcModel(const string prototxt_path, const string weights_path, Net<float>** net)
+{
+    *net = new Net<float>(prototxt_path, caffe::TRAIN);
+    (*net)->CopyTrainedLayersFrom(weights_path);
+    return true;
+}
+
+bool MergeModelClass::copyWeights(Net<float>* src_net, const string src_layer_name, const string dst_layer_name)
+{
+    LOG(INFO) << "Copying " << src_layer_name << " to " << dst_layer_name;
+    const shared_ptr<Layer<float> > src_layer = src_net->layer_by_name(src_layer_name);
+    if (src_layer == NULL)
+    {
+        LOG(ERROR) << "src net has no " << src_layer_name;
+        return false;
+    }
+    const shared_ptr<Layer<float> > dst_layer = dst_net->layer_by_name(dst_layer_name);
+    if (dst_layer == NULL)
+    {
+        LOG(ERROR) << "dst net has no " << dst_layer_name;
+        return false;
+    }
+    
+    if (src_layer->blobs().size() != dst_layer->blobs().size())
+    {
+        LOG(ERROR) << "src layer " << src_layer_name
+            << " and dst layer " << dst_layer_name
+            << " have different number of blobs: "
+            << src_layer->blobs().size() << " != " << dst_layer->blobs().size();
+        return false;
+    }
+
+    for (int b = 0; b < src_layer->blobs().size(); b++)
+    {
+        string src_blob_shape = src_layer->blobs()[b]->shape_string();
+        string dst_blob_shape = dst_layer->blobs()[b]->shape_string();
+        if (src_blob_shape != dst_blob_shape)
+        {
+            LOG(ERROR) << "src layer " << src_layer_name
+                << " and dst layer " << dst_layer_name
+                << " have different shape of blob "
+                << src_blob_shape << " != " << dst_blob_shape;
+            return false;
+        }
+
+        memcpy(dst_layer->blobs()[b]->mutable_cpu_data(),
+            src_layer->blobs()[b]->cpu_data(),
+            sizeof(float)*(src_layer->blobs()[b]->count()));
+    }
+    return true;
+}
+
+void MergeModelClass::addAutoCopyLayers(Net<float>* src_net, vector<SrcDstPair>& pairs)
+{
+    for (int l = 0; l < dst_net->layer_names().size(); l++)
+    {
+        string layer_name = dst_net->layer_names()[l];
+        if (src_net->has_layer(layer_name))
+        {
+            SrcDstPair a_pair;
+            a_pair.src_layer_name = layer_name;
+            a_pair.dst_layer_name = layer_name;
+            pairs.push_back(a_pair);
+        }            
+    }
+}
+
+bool MergeModelClass::copyWeights(Net<float>* src_net, vector<SrcDstPair> pairs, bool auto_flag)
+{    
+    if (auto_flag)
+        addAutoCopyLayers(src_net, pairs);
+    for (int i = 0; i < pairs.size(); i++)
+    {
+        if (!copyWeights(src_net, pairs[i].src_layer_name, pairs[i].dst_layer_name))
+            return false;
+    }
+    return true;
+}
+
+bool MergeModelClass::mergeModel()
+{
+    if (!dst_net_init)
+    {
+        LOG(ERROR) << "dst_net needs to be initialized first";
+        return false;
+    }
+
+    for (int i = 0; i < config_info.src_info.size(); i++)
+    {
+        bool flag_auto = config_info.src_info[i].flag_auto;
+        string prototxt_path = config_info.src_info[i].prototxt_path;
+        string weights_path = config_info.src_info[i].weights_path;
+        Net<float>* src_net = NULL;
+        initSrcModel(prototxt_path, weights_path, &src_net);
+        LOG(INFO) << "Start to copy weights from " << prototxt_path;
+        copyWeights(src_net, config_info.src_info[i].pairs, flag_auto);
+    }
+    NetParameter net_param;
+    dst_net->ToProto(&net_param, false);
+    caffe::WriteProtoToBinaryFile(net_param, config_info.dst_model_path);
     return true;
 }
--- mergeModel.h
+++ mergeModel.h
@@ -11,24 +11,26 @@
 
 using caffe::Blob;
 using caffe::Caffe;
-using caffe::Datum;
+using caffe::Layer;
 using caffe::Net;
+using caffe::NetParameter;
 using std::string;
 using std::ifstream;
 using std::vector;
+using boost::shared_ptr;
 
 //config file:
 //dst_prototxt, dst_model
 //src_prototxt_0 src_model_0 flag_auto n pair_0(src:dst) pair_1
 //src_prototxt_1 src_prototxt_1 flag_auto n pair_0 pair_1
 
-struct SrcDstPair
+typedef struct SrcDstPair
 {
     string src_layer_name;
     string dst_layer_name;
 };
 
-struct SrcInfo
+typedef struct SrcInfo
 {
     bool flag_auto;    //if true: auto fill weights when src and dst layer names are same 
     string prototxt_path;   //src prototxt path
@@ -36,7 +38,7 @@ struct SrcInfo
     vector<SrcDstPair> pairs;   //src layer name - dst layer name
 };
 
-struct ConfigInfo
+typedef struct ConfigInfo
 {
     string dst_prototxt_path; //wanted prototxt path
     string dst_model_path;  //output weights path
@@ -44,6 +46,37 @@ struct ConfigInfo
     vector<SrcInfo> src_info;
 };
 
-bool splitPair(const string input, SrcDstPair& a_pair);
-bool splitSrcLine(const string input, SrcInfo& a_src_info);
-bool loadConfigFile(const string config_file_path, ConfigInfo& config_info);
+class MergeModelClass
+{
+public:
+    MergeModelClass(const string config_file_path)
+    { 
+        dst_net = NULL;
+        if(loadConfigFile(config_file_path))
+            dst_net_init = initDstModel();
+    };
+    ~MergeModelClass()
+    {
+        if (dst_net_init)
+            delete dst_net;
+    };
+    
+    bool mergeModel();
+    
+
+private:
+    bool loadConfigFile(const string config_file_path);
+
+    bool splitPair(const string input, SrcDstPair& a_pair);
+    bool splitSrcLine(const string input, SrcInfo& a_src_info);
+    bool initDstModel();
+    bool initSrcModel(const string prototxt_path, const string weights_path, Net<float>** net);
+    bool copyWeights(Net<float>* src_net, vector<SrcDstPair> pairs, bool auto_flag);
+    bool copyWeights(Net<float>* src_net, const string src_layer_name, const string dst_layer_name);
+    void addAutoCopyLayers(Net<float>* src_net, vector<SrcDstPair>& pairs);
+
+    Net<float>* dst_net;
+    ConfigInfo config_info;
+
+    bool dst_net_init;
+};
